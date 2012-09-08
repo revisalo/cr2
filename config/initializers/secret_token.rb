98efe753d8dfda84c4e2ff601cc79d594a2918c7
afredondo233 <af.redondo233@uniandes.edu.co>
2012-09-08 16:16:09 -0500

    Primer push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cr2::Application.config.secret_token = 'ae9a228b8afb972793f1541942e0877c6881e0c2cfda483a84b1848763a944bcce34e4ca872b6b1bce80b1630e1469ec09b2a5508f418d61eac7da72617e60a7'
