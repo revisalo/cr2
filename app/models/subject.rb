159cc02c1a6d5a8a6c28030f0a385701de15e830
Laura Lunita Lopez <lunicirus@gmail.com>
2012-09-15 17:47:22 -0500

    Revert "foldersubjects"
    This reverts commit 80126fce5609a0fc03f11a545f20972f79981d03.

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,7 +2,6 @@ class Subject < ActiveRecord::Base
   attr_accessible :code, :credits, :name
 
   belongs_to :pensum
-  has_and_belongs_to_many :folders
 
   #Validar todos los campos requeridos
   validates :code, :credits, :name, :presence => { :message => "Todos los campos son requeridos" }
