81469bc3bf292f38ca2dbe7d5e2fc35d92906b93
Julian <j.viveros72@gmail.com>
2012-09-17 13:32:40 -0500

    home -> coordinador

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120915225339) do
+ActiveRecord::Schema.define(:version => 20120908212946) do
 
   create_table "folders", :force => true do |t|
     t.integer  "year"
@@ -21,11 +21,6 @@ ActiveRecord::Schema.define(:version => 20120915225339) do
     t.integer  "semester"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-  end
-
-  create_table "folders_subjects", :id => false, :force => true do |t|
-    t.integer "subject_id"
-    t.integer "folder_id"
   end
 
   create_table "magisters", :force => true do |t|
@@ -54,6 +49,7 @@ ActiveRecord::Schema.define(:version => 20120915225339) do
     t.string   "code"
     t.string   "name"
     t.integer  "credits"
+    t.string   "folder_id"
     t.string   "pensum_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
