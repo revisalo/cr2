e7b3d5993d4ee0901b138e52388b146103001d51
an3sarmiento <a.sarmiento64@uniandes.edu.co>
2012-11-14 09:45:20 -0500

    bash

--- bash2.sh
+++ bash2.sh
@@ -1,6 +1,6 @@
 export GUROBI_HOME="./opt/gurobi501/linux64"
 export PATH="${PATH}:${GUROBI_HOME}/bin"
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
-export GRB_LICENSE_FILE=/home/san/gurobi.lic
+export GRB_LICENSE_FILE="./opt/gurobi.lic"
 
 /usr/lib/jvm/default-java/jre/bin/java -jar gurobisalo.jar ./files/
